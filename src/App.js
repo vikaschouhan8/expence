c1b365777597eee48cf6bd980f516d1a1aef70a3
vikaschouhan8 <aboutvikaschouhan@gmai.com>
2019-10-16 19:05:07 +0530

    add expence table

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Link, Route, Switch } from "react-router-dom"
+import { Link, Route, Switch } from "react-router-dom"
 import GamesPage from './GamesPage';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Link to="/games">Games</Link>
+        <h2>HTML Table</h2>
+
+        <table>
+          <tr>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Mode of money transfer</th>
+            <th>Amount</th>
+          </tr>
+          <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+            <td>Germany</td>
+          </tr>
+          <tr>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+            <td>Amoiunt</td>
+          </tr>
+          <tr>
+            <td>Ernst Handel</td>
+            <td>Roland Mendel</td>
+            <td>Austria</td>
+            <td>Amoiunt</td>
+          </tr>
+          <tr>
+            <td>Island Trading</td>
+            <td>Helen Bennett</td>
+            <td>UK</td>
+            <td>Amoiunt</td>
+          </tr>
+          <tr>
+            <td>Laughing Bacchus Winecellars</td>
+            <td>Yoshi Tannamuri</td>
+            <td>Canada</td>
+            <td>Amoiunt</td>
+          </tr>
+          <tr>
+            <td>Magazzini Alimentari Riuniti</td>
+            <td>Giovanni Rovelli</td>
+            <td>Italy</td>
+            <td>Amoiunt</td>
+          </tr>
+        </table>
+        {/* <Link to="/games">Games</Link> */}
       </header>
       {/* <Switch> */}
-        <Route exact strict path="/games" component={GamesPage} />
+      {/* <Route exact strict path="/games" component={GamesPage} /> */}
       {/* </Switch> */}
     </div>
   );
