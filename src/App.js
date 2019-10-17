80d807aa605928a135061595c02e8ed4675a5c0e
vikaschouhan8 <aboutvikaschouhan@gmai.com>
2019-10-17 11:22:20 +0530

    add table

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Link, Route, Switch } from "react-router-dom"
+import ExpenseTable from './components/ExpenseTable';
 // import GamesPage from './GamesPage';
 function App() {
   return (
@@ -9,50 +10,7 @@ function App() {
       <header className="App-header">
         <h2>HTML Table</h2>
 
-        <table>
-          <tr>
-            <th>Date</th>
-            <th>Description</th>
-            <th>Mode of money transfer</th>
-            <th>Amount</th>
-          </tr>
-          <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>Maria Anders</td>
-            <td>Germany</td>
-            <td>Germany</td>
-          </tr>
-          <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>Francisco Chang</td>
-            <td>Mexico</td>
-            <td>Amoiunt</td>
-          </tr>
-          <tr>
-            <td>Ernst Handel</td>
-            <td>Roland Mendel</td>
-            <td>Austria</td>
-            <td>Amoiunt</td>
-          </tr>
-          <tr>
-            <td>Island Trading</td>
-            <td>Helen Bennett</td>
-            <td>UK</td>
-            <td>Amoiunt</td>
-          </tr>
-          <tr>
-            <td>Laughing Bacchus Winecellars</td>
-            <td>Yoshi Tannamuri</td>
-            <td>Canada</td>
-            <td>Amoiunt</td>
-          </tr>
-          <tr>
-            <td>Magazzini Alimentari Riuniti</td>
-            <td>Giovanni Rovelli</td>
-            <td>Italy</td>
-            <td>Amoiunt</td>
-          </tr>
-        </table>
+        <ExpenseTable />
         {/* <Link to="/games">Games</Link> */}
       </header>
       {/* <Switch> */}
